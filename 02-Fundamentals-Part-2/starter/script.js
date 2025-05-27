8c02b7a06b41f905f26b0649b71434ad84412ae5
Rafael Pinto <pintorafael1999@gmail.com>
2025-05-27 11:53:31 +0100

    Functions calling other functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -46,4 +46,33 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1999);
 
 console.log(age1, age2);
+
+
+// Arrow function
+const calcAge3 = birthYear => 2025 - birthYear;
+const age3 = calcAge3(1999);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2025 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1999, "Rafael"));
+console.log(yearsUntilRetirement(1991, "Bob"));
 */
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
