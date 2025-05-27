cb4e46051dec6afdb8c14dc693fb60ffbb20a249
Rafael Pinto <pintorafael1999@gmail.com>
2025-05-27 10:41:02 +0100

    Functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+// Some private words
+// const interface = "Audio";
+// const private = 534;
+// const if = 23;
+
+function logger() {
+    console.log(`My name is Rafael`);
+}
+// calling / invoking / running function
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
