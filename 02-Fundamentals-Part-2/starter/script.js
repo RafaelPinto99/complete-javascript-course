9384f3eec10acadbadd9e66636cf65442fe02615
Rafael Pinto <pintorafael1999@gmail.com>
2025-05-28 23:13:18 +0100

    Setup Prettier

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -320,10 +320,12 @@ while (rep <= 10) {
     rep++;
 }
 
-*/
+
 let dice = Math.trunc(Math.random() * 6) + 1;
 while (dice !== 6) {
-    console.log(`Dice number: ${dice}`);
-    dice = Math.trunc(Math.random() * 6) + 1;
-    if (dice === 6) console.log("Loop is about to end...");
+  console.log(`Dice number: ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end...");
 }
+
+*/
