8a4cc98a89816408e229fc865edc2421ef977df6
Rafael Pinto <pintorafael1999@gmail.com>
2025-06-13 13:09:41 +0100

    Spread operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -50,7 +50,7 @@ const restaurant = {
   },
 
   orderDelivary: function ({
-    // Parameterers with default values
+    // 1 Parameter which is an object set with default values
     starterIndex = 1,
     mainIndex = 0,
     time = '20.00',
@@ -62,21 +62,16 @@ const restaurant = {
       ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
 
-restaurant.orderDelivary({
-  time: '22.38',
-  address: 'Via del Sole, 21',
-  mainIndex: 2,
-  starterIndex: 2,
-});
-
-restaurant.orderDelivary({
-  address: 'Via del Sole, 21',
-  starterIndex: 1,
-});
-
 /*
+// Destructuring arrays
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -113,6 +108,20 @@ const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
 */
 
+/*
+// Destructuring objects
+restaurant.orderDelivary({
+  time: '22.38',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivary({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+});
+
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
 
@@ -140,3 +149,53 @@ const {
   fri: { open, close },
 } = openingHours;
 console.log(open, close);
+*/
+
+/*
+// Spread operator
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newGoodArr = [1, 2, ...arr];
+console.log(newGoodArr);
+
+console.log(...newGoodArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const maineMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Rafael';
+const letters = [...str, '', 'P.'];
+console.log(letters);
+console.log(...str);
+
+// Real worlds example
+const ingredients = [
+  prompt("Let's make pasta! Ingredient 1?"),
+  prompt('Ingredient 2?'),
+  prompt('Ingredient 3?'),
+];
+
+console.log(ingredients);
+
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name); 
+*/
