3c06c10688f5542e1e4fb3d6a08da9fab376bb58
Rafael Pinto <pintorafael1999@gmail.com>
2025-06-16 19:01:35 +0100

    Values vs Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -28,3 +28,30 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
 */
+
+/*
+const flight = 'LH234';
+const rafael = {
+  name: 'Rafael Pinto',
+  passport: 24739479284,
+};
+
+const checkIn = function (flighNum, passenger) {
+  flighNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) console.log('Checked in');
+  else console.log('Wrong passport');
+};
+
+checkIn(flight, rafael);
+console.log(flight);
+console.log(rafael);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000);
+};
+
+newPassport(rafael);
+checkIn(flight, rafael);
+*/
