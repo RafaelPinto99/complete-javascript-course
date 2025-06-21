ebd9245135281ecf0586b5ef6b2a7ec03249cf57
Rafael Pinto <pintorafael1999@gmail.com>
2025-06-21 14:26:13 +0100

    At method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -107,3 +107,15 @@ console.log([...arr, ...arr2]);
 // Join
 console.log(letters.join(' - '));
 */
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// Getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('Rafael'.at(0));
+console.log('Rafael'.at(-1));
