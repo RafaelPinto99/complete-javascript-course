61e94005d66051fc034fd7c2451ca4d86a592025
Rafael Pinto <pintorafael1999@gmail.com>
2025-06-23 21:42:17 +0100

    Challenge 4

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -179,6 +179,43 @@ btnTransfer.addEventListener('click', function (e) {
   updateUI(currentAccount);
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount / 10)) {
+    // Add movement
+    currentAccount.movements.push(amounts);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = inputClosePin.value = '';
+});
+
+// Find index method
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    (inputCloseUsername,
+    value === currentAccount.username &&
+      Number(inputClosePin.value) === currentAccount.pin)
+  ) {
+    const index = accounts.findIndex(acc => acc.username === currentAccount);
+    console.log(index);
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value;
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -361,43 +398,6 @@ const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 console.log(account);
 */
 
-btnLoan.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  const amount = Number(inputLoanAmount.value);
-
-  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount / 10)) {
-    // Add movement
-    currentAccount.movements.push(amounts);
-
-    // Update UI
-    updateUI(currentAccount);
-  }
-  inputLoanAmount.value = inputClosePin.value = '';
-});
-
-// Find index method
-btnClose.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  if (
-    (inputCloseUsername,
-    value === currentAccount.username &&
-      Number(inputClosePin.value) === currentAccount.pin)
-  ) {
-    const index = accounts.findIndex(acc => acc.username === currentAccount);
-    console.log(index);
-
-    // Delete account
-    accounts.splice(index, 1);
-
-    // Hide UI
-    containerApp.style.opacity = 0;
-  }
-
-  inputCloseUsername.value = inputClosePin.value;
-});
-
 /*
 // Find last and find last index method
 console.log(movements);
@@ -459,6 +459,8 @@ const overallBalance2 = accounts
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overallBalance);
 */
+
+
 
 /*
 // Coding Challenge #1
@@ -571,4 +573,114 @@ const calcAverageHumanAge = ages =>
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
+*/
+
+/*
+// Coding Challenge #4
+
+This time, Julia and Kate are studying the activity levels of different dog breeds.
+
+YOUR TASKS:
+1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+3. Create an array "allActivities" of all the activities of all the dog breeds
+4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions).
+   HINT: Use a technique with a special data structure that we studied a few sections ago.
+5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do,
+   in a unique array called "swimmingAdjacent".
+6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
+
+TEST DATA:
+*/
+
+/*
+const breeds = [
+  {
+    breed: 'German Shepherd',
+    averageWeight: 32,
+    activities: ['fetch', 'swimming'],
+  },
+  {
+    breed: 'Dalmatian',
+    averageWeight: 24,
+    activities: ['running', 'fetch', 'agility'],
+  },
+  {
+    breed: 'Labrador',
+    averageWeight: 28,
+    activities: ['swimming', 'fetch'],
+  },
+  {
+    breed: 'Beagle',
+    averageWeight: 12,
+    activities: ['digging', 'fetch'],
+  },
+  {
+    breed: 'Husky',
+    averageWeight: 26,
+    activities: ['running', 'agility', 'swimming'],
+  },
+  {
+    breed: 'Bulldog',
+    averageWeight: 36,
+    activities: ['sleeping'],
+  },
+  {
+    breed: 'Poodle',
+    averageWeight: 18,
+    activities: ['agility', 'fetch'],
+  },
+];
+
+// 1.
+const huskyWeight = breeds.find(dog => dog.breed === 'Husky').averageWeight;
+console.log(huskyWeight);
+
+// 2.
+const dogBothActivities = breeds.find(
+  dog => dog.activities.includes('running') && dog.activities.includes('fetch')
+).breed;
+console.log(dogBothActivities);
+
+// 3.
+const allActivities = breeds.flatMap(dog => dog.activities);
+~console.log(allActivities);
+
+// 4.
+const uniqueActivities = [...new Set(allActivities)];
+console.log(uniqueActivities);
+
+// 5;
+const swimmingAdjacent = [
+  ...new Set(
+    breeds
+      .filter(dog => dog.activities.includes('swimming'))
+      .flatMap(dog =>
+        dog.activities.filter(activity => activity !== 'swimming')
+      )
+  ),
+];
+console.log(swimmingAdjacent);
+
+// 6.
+console.log(breeds.every(dog => dog.averageWeight > 10));
+
+// 7.
+console.log(breeds.some(dog => dog.activities.length >= 3));
+
+// BONUS
+// const heaviestFetcherWeight = breeds
+//   .filter(dog => dog.activities.includes('fetch'))
+//   .reduce((max, dog) => (dog.averageWeight > max ? dog.averageWeight : max), 0);
+
+const fetchWeights = breeds
+  .filter(dog => dog.activities.includes('fetch'))
+  .map(dog => dog.averageWeight); // remember map returns an array
+console.log(fetchWeights);
+const heaviestFetcherWeight = Math.max(...fetchWeights);
+
+console.log(heaviestFetcherWeight);
 */
