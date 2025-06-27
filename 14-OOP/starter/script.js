532f3176ae64d9b0e96b2be3083e5aca01bdbe78
Rafael Pinto <pintorafael1999@gmail.com>
2025-06-27 16:33:36 +0100

    Object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -118,3 +118,28 @@ jessica.greet();
 // 3. Classes are executed in strict mode
 */
 
+// Object.create
+/*
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
+*/
