a09ca665adc68e71ed6adac50edf3ce938ec3ac0
Rafael Pinto <pintorafael1999@gmail.com>
2025-06-27 16:15:56 +0100

    Prototypal Inheritance on Built-In Objects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -49,3 +49,27 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
+
+// Prototypal Inheritance on Built-In Objects
+console.log('---- X ----');
+console.log(jonas.__proto__);
+// Object.protoype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 4, 5, 6, 9, 9, 3]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
